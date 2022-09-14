cb3d685221cbc86407f2b25b76f7c0a42d7ec7b7
YaSh88991 <vermayash88991@gmail.com>
2022-09-14 12:45:33 +0530

    regular changes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const log = require('electron-log')
 
 
 log.transports.file.resolvePath=()=> __dirname + '/logs/log.log'
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'production'
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
